b7297736bb0037a4be6256a2e17e7e50d682932d
Patrick Azevedo <patrick.calazans@hotmail.com>
2024-03-28 20:56:35 -0300

    js-atividade01

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = prompt('Digite o seu nome: ')
+let sobrenome = prompt('Digite o seu sobrenome: ')
+
+let ano = Number(prompt('Digite o ano em que você nasceu: '))
+let anoTotal = 2024 - ano
+
+alert(`Olá ${nome} ${sobrenome}, você já fez ou vai fazer ${anoTotal} anos!`)
